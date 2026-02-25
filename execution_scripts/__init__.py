835797102b19687c66016209bfb3ece189236b03
Natinael Neggatu <nati@MacBookPro.lan>
2026-02-25 19:07:07 +0000

    adding more code for handling compatibility

--- execution_scripts/__init__.py
+++ execution_scripts/__init__.py
@@ -1,3 +1,7 @@
+from utils.compat import apply_runtime_compat_patches
+
+apply_runtime_compat_patches()
+
 from .td3_n import td3_n_offline
 from .bc import bc_offline
 from .combined import combined
